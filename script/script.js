3a5226ac648eabf39804a3b70b3d8931af4c0af7
Pedro Nepomuceno <pedrohnlima@gmail.com>
2021-12-12 20:57:24 -0800

    feat: DOM

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+// toggle elements 
+
+const popup = document.querySelector('.popup')
+const popupClose = document.querySelector('.popup')
+const inputName = document.querySelector('.popup__input_type_name')
+const inputDescription = document.querySelector('.popup__input_type_description')
+const popupSubmit = document.querySelector('.popup__submit')
+
+// functions 
+
+function popup (){
+    
+}
