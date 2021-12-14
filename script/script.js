817be4584e077e832bf8f56784569e23308f34a9
Pedro Nepomuceno <pedrohnlima@gmail.com>
2021-12-13 19:20:17 -0800

    feat: popup js

--- script/script.js
+++ script/script.js
@@ -1,14 +1,40 @@
 
 // toggle elements 
 
-const popup = document.querySelector('.popup')
-const popupClose = document.querySelector('.popup')
-const inputName = document.querySelector('.popup__input_type_name')
-const inputDescription = document.querySelector('.popup__input_type_description')
-const popupSubmit = document.querySelector('.popup__submit')
+const modalWindow = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__form');
+
+const popupClose = document.querySelector('.popup__close');
+const popupSubmit = document.querySelector('.popup__submit');
+const editButton = document.querySelector('.profile__edit');
+
+
+const inputName = document.querySelector('.popup__input_type_name');
+const inputDescription = document.querySelector('.popup__input_type_description');
+const profileName = document.querySelector('.profile__name');
+const profileText = document.querySelector('.profile__text');
 
 // functions 
 
-function popup (){
-    
+function toggleModal (){
+    if(!modalWindow.classList.contains("popup_open")){
+inputName.value = profileName.textContent;
+inputDescription.value = profileText.textContent;
+    }
+
+modalWindow.classList.toggle("popup_open");
 }
+ 
+
+function formSubmit (evt){
+evt.preventDefault();
+ profileName.textContent = inputName.value;
+ profileText.textContent = inputDescription.value;
+
+toggleModal();
+}
+// event listener 
+
+popupForm.addEventListener('submit', formSubmit);
+editButton.addEventListener('click', toggleModal);
+popupClose.addEventListener('click', toggleModal);
