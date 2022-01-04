b850c8300961df40d511d38b641c2912d91015b7
Pedro Nepomuceno <pedrohnlima@gmail.com>
2022-01-03 19:16:24 -0800

    feat: js

--- script/script.js
+++ script/script.js
@@ -3,18 +3,31 @@
 
 const modalWindow = document.querySelector('.popup');
 const popupForm = document.querySelector('.popup__form');
-
 const popupClose = document.querySelector('.popup__close');
 const popupSubmit = document.querySelector('.popup__submit');
 const editButton = document.querySelector('.profile__edit');
-
-
+const addClose = document.querySelector('.add__close');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDescription = document.querySelector('.popup__input_type_description');
 const profileName = document.querySelector('.profile__name');
 const profileText = document.querySelector('.profile__text');
-
+const addPhoto = document.querySelector('.add');
+const buttonAdd = document.querySelector('.profile__plus');
+const addInputName = document.querySelector('.add__input_type_name');
+const addInputDescription = document.querySelector('.add__input_type_name');
+const likeButton = document.querySelector('.elements__info-button');
 // functions 
+function blackHeart (){
+    likeButton.classList.toggle('elements__info-button_active')
+}
+
+function newPhoto (){
+    if(!addPhoto.classList.contains('add_open')){
+        addInputName.value = "Title";
+        addInputDescription.value = "image link";
+    }
+    addPhoto.classList.toggle("add_open");
+}
 
 function toggleModal (){
     if(!modalWindow.classList.contains("popup_open")){
@@ -33,8 +46,12 @@ evt.preventDefault();
 
 toggleModal();
 }
-// event listener 
 
+
+// event listener
+likeButton.addEventListener('click', blackHeart); 
+buttonAdd.addEventListener('click',newPhoto);
+addClose.addEventListener('click', newPhoto);
 popupForm.addEventListener('submit', formSubmit);
 editButton.addEventListener('click', toggleModal);
 popupClose.addEventListener('click', toggleModal);
