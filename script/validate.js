ac9641be763484245933c85e7d0d740f6a786985
Pedro Nepomuceno <pedrohnlima@gmail.com>
2022-01-21 17:56:54 -0800

    feat: validation js

---
+++ script/validate.js
@@ -0,0 +1,11 @@
+const formSelector = document.querySelectorAll(".popup__form");
+const inputSelector = document.querySelectorAll(".popup__input");
+const  submitButtonSelector = document.querySelectorAll(".popup__submit");
+const inactiveButtonClass = document.querySelectorAll(".popup__button_disabled");
+const inputErrorClass = document.querySelectorAll(".popup__input_type_error");
+const errorClass = document.querySelectorAll(".popup__error_visible");
+
+
+
+
+
