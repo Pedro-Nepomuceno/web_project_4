e1f51020e90f10a3b087079c11477b5b526f9613
Pedro Nepomuceno <pedrohnlima@gmail.com>
2022-01-24 12:42:29 -0800

    fix js validation

--- script/validate.js
+++ script/validate.js
@@ -5,7 +5,7 @@ const inputErrorClass = document.querySelectorAll(".popup__input_type_error");
 const errorClass = document.querySelectorAll(".popup__error_visible");
 
 const inputElement = document.querySelectorAll(".popup__input");
-
+const formElement = document.querySelectorAll(".popup__form");
 
 
 const isValid = (formElement,inputElement) => {
@@ -20,23 +20,23 @@ const isValid = (formElement,inputElement) => {
 
 const showInputError = (formElement, inputElement, errorMessage) => {
   
-    const errorElement = document.querySelector("#name-error");
-    inputElement.classList.add("form__input_type_error");
+    const errorElement = document.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.add("popup__input-error");
     errorElement.textContent = errorMessage;
-    errorElement.classList.add("form__input-error-active");
+    errorElement.classList.add("popup__input-error-active");
   };
 
 const hideInputError = (formElement, inputElement) => {
-  const errorElement = document.querySelector(`.${inputElement.id}-error`);
- 
-  inputElement.classList.remove("form__input_type_error");
-  errorElement.classList.remove("form__input-error-active");
+
+    const errorElement = document.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove("popup__input-error");
+  errorElement.classList.remove("popup__input-error-active");
   errorElement.textContent = "";
 }; 
 
 const setEventListeners = (inputElement) => {
    
-    const formElement = document.querySelectorAll(".popup__form");
+    
     inputList = Array.from(inputElement)
 console.log(inputList);
     inputList.forEach((inputElement) =>{
